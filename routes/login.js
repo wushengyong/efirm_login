1d34a60e56a4fa127ce88f1324f9d6626b2f0669
wushengyong <375228075@qq.com>
2017-07-05 15:27:03 +0800

    1、修改login.ejs和login.js 2、修改style.css使表单居中，及错误消息的颜色。

--- routes/login.js
+++ routes/login.js
@@ -6,16 +6,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var context = {
-        title : 'login',
-        welcome_message : 'Login',
-        fields : [
-            {name:'username',type : 'text', placeholder : 'username'},
-            {name:'password',type : 'password', placeholder : 'password'}
-        ],
-        default_value : [{name:'username',value :''}, {name : 'password', value : ''}]
-    }
-    res.render('login', context);
+    res.render('login', { title : 'login'});
 });
-
+router.post('/',function (req, res, next) {
+    res.send("username : " + req.body.username + " password : " + req.body.password);
+});
 module.exports = router;
